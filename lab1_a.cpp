8c5e29f6243d04abc2300e6e3d4d39f4c45249ac
6504062610242salankosol <s6504062610242@email.kmutnb.ac.th>
2025-12-09 13:53:44 +0700

    Add files via upload

---
+++ lab1_a.cpp
@@ -0,0 +1,74 @@
+
+/*
+#include<stdio.h>
+int main()
+{
+	int h=0,m=0,s=0;
+	char c1,c2;
+	scanf("%d %c %d %c %d",&h,&c1,&m,&c2,&s);
+	printf("%d%c%d%c%d",h,c1,m,c2,s);
+}*/
+
+/*
+#include<stdio.h>
+int main()
+{
+	int n=0,sum=0;
+	
+	scanf("%d",&n);
+	
+	int a[n];
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	
+	for(int i=0;i<n;i++)
+	{
+		sum += a[i];
+	}
+	float discount=0,getdis=0,total=0,vat=0,totalvat=0;
+	
+	if(n==1)
+	{
+		discount = 5.0;
+		discount = discount/100;
+		
+		getdis = sum*discount;
+		total = sum-getdis;
+		
+		vat = total*0.07;
+		totalvat = total+vat;
+		
+		printf("%.1f",total);
+	}
+	else if(n==2)
+	{
+		discount = 15.0;
+		discount = discount/100;
+		
+		getdis = sum*discount;
+		total = sum-getdis;
+		
+		vat = total*0.07;
+		totalvat = total+vat;
+		
+		printf("%.1f",total);
+	}
+	else if(n==3)
+	{
+		discount = 30.0;
+		discount = discount/100;
+		
+		getdis = sum*discount;
+		total = sum-getdis;
+		
+		vat = total*0.07;
+		totalvat = total+vat;
+		
+		printf("%.1f",total);
+	}
+	
+}
+*/
+
